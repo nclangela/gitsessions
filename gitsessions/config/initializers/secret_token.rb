ca9f2265c2917eaf3c2c9691bbb781df7fde9ed9
nclangela <nclangela.rvr@gmail.com>
2013-05-22 14:02:30 +0800

    initial commit

---
+++ gitsessions/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitsessions::Application.config.secret_token = '7435bfcebc07b0a58022b55e9acf516810d7bf785b5abfe95cc5e03ab2cd396666161e9bae5461c637c7d31a666f9da57a192415145745a196ba3b6261550fa4'
